76db11c66ae1d8a4e176be33d5c78491b0051253
Brandon <bfigueroa004@student.butte.edu>
2016-08-29 06:51:10 +0000

    lab1.cpp

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//          Mr senior Robertõ
+int main() {
+std::cout << " ( _ ~ ) " << std::endl;
+std::cout << "| o   O |" << std::endl;
+std::cout << "|  <>   |" << std::endl;
+std::cout << "| ~~~~  |" << std::endl;
+std::cout << " ````| |` " << std::endl;
+std::cout << "x''''| |''''x" << std::endl; 
+std::cout << "      V    " << std::endl; 
+}                 
+ 
+        
