f72aad703645703273d05a580ed035b70e57117d
Brandon <bfigueroa004@student.butte.edu>
2016-08-29 08:37:43 +0000

    lab2.cpp

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -1,15 +1,14 @@
 
-/*The number is 17 from the range of 1 through 20
-
-/*If user input is 17; award 10 points
-/*If not 17; deduct 1 point 
-//If user input is higher than 17; print out "too high!"
-//If lower than 17; print out "too low!"
+//The number is 17 from the range of 1 through 20 (randomly selected)
+//User starts with 10 points
+//If user input is 17; user wins and is awarded 10 points
+//If not 17; deduct 1 point from a the starting 10 points 
+//If user input is higher than 17; print out "higher!"
+//If lower than 17; print out "lower!"
 //Allow user to try again
 //If user input is 17; award 10 points
-//If not 17; deduct 1 point
+//If not 17; deduct 1 point from new score
 //If user input is higher than 17; print out "too high!"
-//If lower than 17; print out "too low!"
 //Repeat lines 7-11 until user guesses correctly.
 
 
