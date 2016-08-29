dadee5298accc9db5efdff8871b4f7f91a264e03
Brandon <bfigueroa004@student.butte.edu>
2016-08-29 08:34:46 +0000

    lab2.cpp

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,15 @@
+
+/*The number is 17 from the range of 1 through 20
+
+/*If user input is 17; award 10 points
+/*If not 17; deduct 1 point 
+//If user input is higher than 17; print out "too high!"
+//If lower than 17; print out "too low!"
+//Allow user to try again
+//If user input is 17; award 10 points
+//If not 17; deduct 1 point
+//If user input is higher than 17; print out "too high!"
+//If lower than 17; print out "too low!"
+//Repeat lines 7-11 until user guesses correctly.
+
+
