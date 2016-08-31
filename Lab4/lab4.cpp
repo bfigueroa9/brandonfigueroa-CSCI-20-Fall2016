a2a5f11b06216d0fe1493519b6f9bbc5b0213252
Brandon <bfigueroa004@student.butte.edu>
2016-08-31 20:30:39 +0000

    lab4.cpp

---
+++ Lab4/lab4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+int main() 
+{
+    string name1;
+    string adjective1;
+    string country1;
+    int number1 = 0; 
+    string adjective2; 
+    float numDecimals;
+    string politician1;
+    string adjective3;
+    string verb1;
+    
+    
+    
+    cout << "Enter your name" << endl;
+    cin  >> name1;
+    
+    cout << "Enter an adjective" << endl;
+    cin  >> adjective1;
+    
+    cout << "Enter a country" << endl;
+    cin  >> country1;
+    
+    cout << "Enter a number" << endl;
+    cin  >> number1;
+    
+    cout << "Enter an adjective" << endl;
+    cin  >> adjective2;
+    
+    cout << "Enter a number with a decimal" <<endl;
+    cin  >> numDecimals;
+    
+    cout << "Enter a politician" << endl;
+    cin  >> politician1;
+    
+    cout << "Enter an adjective" << endl;
+    cin  >> adjective3;
+    
+    cout << "Enter a verb" << endl;
+    cin  >> verb1;
+    cout << endl;
+    
+    cout << name1 << " is on thier way to the " << adjective1 << " bus stop." << endl;
+    cout << name1 << " is going to " << country1 << " to fight " << number1 << " "<< adjective2 << " ninjas!" << endl;
+    cout << "The ninjas were set in the " << numDecimals << " eagle-eye formation." << endl;
+    cout << name1 << " began to cry and called for back up. Thats when " << politician1 << " appeared and " << verb1 << " around the " << adjective3 << " ninjas." << endl;
+    cout << name1 << " and " << politician1 << " defeated the ninjas with thier fame and curruption super powers." << endl;
+}
+    
+
+    
+    
+  
+     
+
+    
+    
+     
